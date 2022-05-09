1792f24f1e1bdf28bc0aeba6458298a0a1ebb4e8
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-09 20:55:46 -0300

    usando dotenv

--- src/http.ts
+++ src/http.ts
@@ -5,12 +5,14 @@ import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-mongoose.connect("mongodb://localhost/rocketsocket");
+// mongoose.connect("mongodb://localhost/rocketsocket");
+mongoose.connect(process.env.MONGO_URL);
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 io.on("connection", (socket) => {
